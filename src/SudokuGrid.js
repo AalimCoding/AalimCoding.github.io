bae866bb47ba3875d412bcd5c3883dc52533c368
Aalim <aalimskhan@gmail.com>
2023-11-09 00:05:44 +0000

    Added to-do list to Sudoku and continued setting up the grid.

--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -4,17 +4,30 @@ function SudokuGrid() {
   const gridSize = 9; // Number of boxes in the grid
   const itemsPerBox = 9; // Number of items per box
 
+  var SudokuValues = []
+  for (let i = 0; i < gridSize; i++) {
+    SudokuValues.push([])
+    for (let j = 0; j < gridSize; j++) {
+      SudokuValues[i].push([0])
+    }
+  }
+  function handleClick(Box, Item) {
+
+    SudokuValues[Box][Item] = 1
+  }
+
+
   const renderGridItems = () => {
     const gridItems = [];
     for (let i = 0; i < gridSize; i++) {
       const boxItems = [];
       for (let j = 0; j < itemsPerBox; j++) {
         const itemId = `Box ${i + 1} Item ${j + 1}`;
-        boxItems.push(<GridItem aspectRatio='1/1' key={itemId} id={itemId}>x</GridItem>); // Use id instead of ID
+        boxItems.push(<GridItem aspectRatio='1/1' key={itemId} id={itemId} onClick={() => handleClick(i, j)}>{SudokuValues[i][j]}</GridItem>); // Use id instead of ID
       }
       const boxId = `Box ${i + 1}`;
       gridItems.push(
-        <Grid aspectRatio='1/1' key={boxId} id={boxId} templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={2}>
+        <Grid aspectRatio='1/1' key={boxId} id={boxId} templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={5} bg="yellow">
           {boxItems}
         </Grid>
       );
@@ -22,7 +35,16 @@ function SudokuGrid() {
     return gridItems;
   };
 
-  return <Grid id="Main Grid" templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={2} bg="green">{renderGridItems()}</Grid>;
+  return <Grid id="Main Grid" templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={10} bg="green">{renderGridItems()}</Grid>;
+
+
+
+
+
+
+
 }
+
+
 
 export default SudokuGrid;
