406467bdcb7f2cbe28aba4f5fb03b06b65bf5ce3
Aalim <aalimskhan@gmail.com>
2023-11-16 01:54:22 +0000

    Changed Sudoku text colour for cells with non-zero values for easier readability. Added comments to the SudokuGrid component.

--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -1,12 +1,15 @@
+// Importing necessary components and hooks from Chakra UI and React
 import { Grid, GridItem } from "@chakra-ui/react";
 import { useState } from "react";
 import { useTheme } from "./ThemeContext"
 
 function SudokuGrid() {
+  // Getting the current theme using the useTheme hook
   const { theme } = useTheme();
   const gridSize = 9; // Number of boxes in the grid
   const itemsPerBox = 9; // Number of items per box
 
+  // Initializing the Sudoku grid with initial values using useState hook
   const [SudokuValues, setSudokuValues] = useState(() => {
     const initialSudokuValues = Array.from({ length: gridSize }, () =>
       Array(itemsPerBox).fill(0)
@@ -14,10 +17,13 @@ function SudokuGrid() {
     return initialSudokuValues;
   });
 
+  // Handling the click event on a Sudoku cell
   function handleClick(row, column) {
     setSudokuValues((prevValues) => {
+      // Creating a copy of the previous Sudoku values array
       const newValues = prevValues.map((rowValues) => [...rowValues]);
 
+      // Updating the selected cell's value
       if (newValues[row][column] < 9) {
         newValues[row][column] += 1;
       } else {
@@ -28,16 +34,19 @@ function SudokuGrid() {
     });
   }
 
+  // Generating the Sudoku grid items
   const gridItems = [];
   for (let i = 0; i < gridSize; i++) {
     const boxItems = [];
     for (let j = 0; j < itemsPerBox; j++) {
+       // Calculating cell attributes
       const box = i + 1;
       const itemInBox = j + 1;
       const row = Math.floor(j / 3) + 3 * Math.floor(i / 3);
       const column = (i % 3) * 3 + (j % 3);
       const itemId = `Box ${box} ItemInBox ${itemInBox} Row ${row} Column ${column}`;
 
+      // Creating Sudoku cell components
       boxItems.push(
         <GridItem
           row={row + 1}
@@ -66,7 +75,9 @@ function SudokuGrid() {
             theme === "light" ? "white" : "black",
 
 
-            color: theme === "light" ? "black" : "white",
+            color: 
+            SudokuValues[row][column] !== 0 ? "black":
+            theme === "light" ? "black" : "white",
         }}
         >
           {SudokuValues[row][column]}
@@ -75,6 +86,7 @@ function SudokuGrid() {
     }
 
     const boxId = `Box ${i + 1}`;
+    // Creating a 3x3 grid for each Sudoku box
     gridItems.push(
       <Grid
         aspectRatio="1/1"
@@ -93,6 +105,7 @@ function SudokuGrid() {
     );
   }
 
+  // Final main Sudoku grid
   return (
     <Grid
       id="Main Grid"
