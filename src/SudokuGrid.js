46980c6f9e85758991fdc09969236092c172cd2b
Aalim <aalimskhan@gmail.com>
2023-11-19 02:24:18 +0000

    Moved Button to SudokuGrid component. This way the button can utilise the same variables given that it is solving the grid and thus should have always been part of this grid component. Continued working on this solving code.

--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -2,12 +2,26 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { useState } from "react";
 import { useTheme } from "./ThemeContext"
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+/* I use Chakra UI Buttons. For docs go to:
+https://chakra-ui.com/docs/components/button/usage */
+
 
 function SudokuGrid() {
   // Getting the current theme using the useTheme hook
   const { theme } = useTheme();
   const gridSize = 9; // Number of boxes in the grid
   const itemsPerBox = 9; // Number of items per box
+
+
+  var possibleSudokuValues = []
+  for (var i = 0; i < 9; i++) {
+    possibleSudokuValues.push([])
+    for (var j = 0; j < 9; j++) {
+      possibleSudokuValues[i].push([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    }
+  }
 
   // Initializing the Sudoku grid with initial values using useState hook
   const [SudokuValues, setSudokuValues] = useState(() => {
@@ -39,7 +53,7 @@ function SudokuGrid() {
   for (let i = 0; i < gridSize; i++) {
     const boxItems = [];
     for (let j = 0; j < itemsPerBox; j++) {
-       // Calculating cell attributes
+      // Calculating cell attributes
       const box = i + 1;
       const itemInBox = j + 1;
       const row = Math.floor(j / 3) + 3 * Math.floor(i / 3);
@@ -57,28 +71,29 @@ function SudokuGrid() {
           key={itemId}
           id={itemId}
           onClick={() => handleClick(row, column)}
-        
-          style={{cursor: "pointer" ,
-            background: 
-            SudokuValues[row][column] === 1 ? "#FA9189":
-            SudokuValues[row][column] === 2 ? "#FCAE7C":
-            SudokuValues[row][column] === 3 ? "#FFE699":
-            SudokuValues[row][column] === 4 ? "#D1FF99":
-            SudokuValues[row][column] === 5 ? "#B3F5BC":
-            SudokuValues[row][column] === 6 ? "#D6F6FF":
-            SudokuValues[row][column] === 7 ? "#9FD1FF":
-            SudokuValues[row][column] === 8 ? "#E2CBF7":
-            SudokuValues[row][column] === 9 ? "#D1BDFF":
-            
-            
-            
-            theme === "light" ? "white" : "black",
+
+          style={{
+            cursor: "pointer",
+            background:
+              SudokuValues[row][column] === 1 ? "#FA9189" :
+                SudokuValues[row][column] === 2 ? "#FCAE7C" :
+                  SudokuValues[row][column] === 3 ? "#FFE699" :
+                    SudokuValues[row][column] === 4 ? "#D1FF99" :
+                      SudokuValues[row][column] === 5 ? "#B3F5BC" :
+                        SudokuValues[row][column] === 6 ? "#D6F6FF" :
+                          SudokuValues[row][column] === 7 ? "#9FD1FF" :
+                            SudokuValues[row][column] === 8 ? "#E2CBF7" :
+                              SudokuValues[row][column] === 9 ? "#D1BDFF" :
 
 
-            color: 
-            SudokuValues[row][column] !== 0 ? "black":
-            theme === "light" ? "black" : "white",
-        }}
+
+                                theme === "light" ? "white" : "black",
+
+
+            color:
+              SudokuValues[row][column] !== 0 ? "black" :
+                theme === "light" ? "black" : "white",
+          }}
         >
           {SudokuValues[row][column]}
         </GridItem>
@@ -98,27 +113,79 @@ function SudokuGrid() {
         style={{
           background: theme === "light" ? "white" : "black",
           color: theme === "light" ? "black" : "white",
-      }}
+        }}
       >
         {boxItems}
       </Grid>
     );
   }
 
+
+
+
+
+  function handleButtonClick() {
+    // THIS IS THE FUNCTION THAT CHECKS IF AN ANSWER IS VALID FOR A CELL
+
+
+    console.log(possibleSudokuValues)
+
+
+    for (var row = 0; row < 9; row++) {
+      for (var column = 0; column < 9; column++) {
+        if (SudokuValues[row][column] = 1) {
+
+          for (var x = 0; x < 9; x++) {
+            possibleSudokuValues[row][x] = possibleSudokuValues[row][x].filter(value => value !== 1)
+            possibleSudokuValues[x][column] = possibleSudokuValues[x][column].filter(value => value !== 1)
+          }
+        }
+      }
+    }
+  }
+
+
+  console.log(possibleSudokuValues)
+
+
+
   // Final main Sudoku grid
   return (
-    <Grid
-      id="Main Grid"
-      templateColumns="repeat(3, 1fr)"
-      templateRows="repeat(3, 1fr)"
-      gap={10}
-      style={{
-        background: theme === "light" ? "white" : "black",
-        color: theme === "light" ? "black" : "white",
-    }}
-    >
-      {gridItems}
-    </Grid>
+    <div>
+      <Grid
+        id="Main Grid"
+        templateColumns="repeat(3, 1fr)"
+        templateRows="repeat(3, 1fr)"
+        gap={10}
+        style={{
+          background: theme === "light" ? "white" : "black",
+          color: theme === "light" ? "black" : "white",
+        }}
+      >
+        {gridItems}
+      </Grid>
+
+
+
+
+
+      <Button
+        // colorScheme='teal'
+        variant='outline'
+        style={{
+          background: theme === "light" ? "white" : "black",
+          color: theme === "light" ? "black" : "white",
+        }}
+        onClick={handleButtonClick}
+      /*  isLoading
+      loadingText='Submitting' */
+      >
+        This is the Sudoku submit button component
+      </Button>
+
+
+
+    </div>
   );
 }
 
